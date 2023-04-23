2c964002789e0e418f2dff28d5f332fd30ba0af6
JonasCappe <jonas.cap@informaticap.be>
2023-04-23 14:43:54 +0200

    Fixed check parent OU

--- 03_Windows_Active_Directory_Objects/Object_Functions.ps1
+++ 03_Windows_Active_Directory_Objects/Object_Functions.ps1
@@ -29,7 +29,7 @@ function Add-Shares
         [string]$DestinationServer
     );
 
-    $ServerSession = New-PSSession -ComputerName $DestinationServer -Credential  (Get-Credential -Message "Enter credentials for $($DestinationServer)" -UserName "Administrator");
+    $ServerSession = New-PSSession -ComputerName $DestinationServer -Credential (Get-Credential -Message "Enter credentials for $($DestinationServer)" -UserName "Administrator");
 
     # Create the share containing the users home folders. (Tip:New-SmbShare, Get-Acl, SetAccessRuleProtection, Set-Acl, ...)
     $Shares = Import-Csv -Delimiter ";" -Path $SourceFile; # Import Shares from CSV file
@@ -40,27 +40,30 @@ function Add-Shares
             Invoke-Command -ScriptBlock { New-Item -ItemType Directory -Path $args[0] -Force } -ArgumentList $Share.Path -Session $ServerSession;
         }
         #Check if share already exists, if not create it
-        if (-not (Invoke-Command -ScriptBlock { Get-SmbShare -Name $args[0] } -ArgumentList $Share.Name -Session $ServerSession))
+        if (-not (Invoke-Command -ScriptBlock { Get-SmbShare -Name $args[0] -ErrorAction SilentlyContinue } -ArgumentList $Share.Name -Session $ServerSession))
         {
-            New-SmbShare -Name $Share.Name -Path $Share.Path -FullAccess $Share.FolderPermissions -CimSession $ServerSession;
+            Invoke-Command -Session $ServerSession -ScriptBlock { New-SmbShare -Name $using:Share.Name -Path $using:Share.Path -FullAccess $using:Share.FolderPermissions};
         }
         else
         {
             Write-Host "Share $Share.Name already exists";
         }
         # Set ACL of share, remove all access rules and add specified access rules
-        $ACL = Get-Acl -Path "\\$($Infrastructure[0].Name)\$($Share.Name)"; # Get ACL of share
-        $ACL.SetAccessRuleProtection($true, $false); # Disable inheritance
-        $ACL.Access | ForEach-Object { $ACL.RemoveAccessRule($_) } # Remove all access rules
-        #$Security = $Share.NtfsPermission.split(",")[0].Split(":");
-        #$NtfsPermission = $Share.NtfsPermission.split(",")[1].Split(":").replace("|",",");
-        foreach($Permission in $Share.NtfsPermission.split(",")) # Loop through all permissions, to create access rules
-        {
-            $SecurityPrincipal = $Permission.split(":")[0]; # Get Security Principal
-            $NtfsPermission = $Permission.split(":")[1].replace("|",","); # Get Ntfs Permission
-            $ACL.AddAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule($SecurityPrincipal, "$NtfsPermission", "Allow"))); # Create new access rule for Security Principal and add it to ACL
+        Invoke-Command -Session $ServerSession -ScriptBlock { 
+            $ACL = Get-Acl -Path "\\$($using:DestinationServer)\$($using:Share.Name)"; # Get ACL of share
+            $ACL.SetAccessRuleProtection($true, $false); # Disable inheritance
+            $ACL.Access | ForEach-Object { $ACL.RemoveAccessRule($_) } # Remove all access rules
+            $Share = $using:Share;
+            $Security = $Share.NtfsPermission.split(",")[0].Split(":");
+            $NtfsPermission = $Share.NtfsPermission.split(",")[1].Split(":").replace("|",",");
+            foreach($Permission in $Share.NtfsPermission.split(",")) # Loop through all permissions, to create access rules
+            {
+                $SecurityPrincipal = $Permission.split(":")[0]; # Get Security Principal
+                $NtfsPermission = $Permission.split(":")[1].replace("|",","); # Get Ntfs Permission
+                $ACL.AddAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule($SecurityPrincipal, "$NtfsPermission", "Allow"))); # Create new access rule for Security Principal and add it to ACL
+            }
+            Set-Acl -Path "\\$($using:DestinationServer)\$($using:Share.Name)" -AclObject $ACL; # Set ACL of share
         }
-        Set-Acl -Path "\\$($Infrastructure[0].Name)\$($Share.Name)" -AclObject $ACL; # Set ACL of share
     }
     <#
     Sources: 
@@ -104,10 +107,10 @@ function Add-UsersInAD
 
     );
 
-    $ServerSession = New-PSSession -ComputerName $DestinationServer -Credential  (Get-Credential -Message "Enter credentials for $($DestinationServer)" -UserName "Administrator");
+    $ServerSession = New-PSSession -ComputerName $DestinationServer -Credential (Get-Credential -Message "Enter credentials for $($DestinationServer)" -UserName "Administrator");
     $Users = Import-Csv -Delimiter ";" -Path $SourceFile; # Import Users from CSV file
     Invoke-Command -Session $ServerSession -ScriptBlock { 
-        foreach ($User in $using:Users) 
+        foreach ($User in $Users) 
         {
             # Extract data from CSV file
             $Surname = $User.Lastname;
@@ -194,7 +197,6 @@ function Add-GroupsInAD
 }
 
 
-
 function Add-OrganizationalUnits
 {
     <#
@@ -227,7 +229,7 @@ function Add-OrganizationalUnits
     
 
     $Parent = Import-Csv -Delimiter ";" -Path $SourceFile | Select-Object -First 1;
-    $OrganizationalUnits = Import-Csv -Delimiter ";" -Path $SourceFile | Where-Object { $_.Path -ne $Parent.Name };
+    $OrganizationalUnits = Import-Csv -Delimiter ";" -Path $SourceFile | Where-Object { $_.Name -ne $Parent.Name };
 
     if (-not (Get-ADOrganizationalUnit -Filter { DistinguishedName -like "OU='$($Parent.Name)','$($DistinguishedPath)'" } -ErrorAction SilentlyContinue)) # If Organizational Unit exists, skip
     {
